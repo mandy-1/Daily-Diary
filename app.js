5addf362487c6d81807c2f127d8f711bf83d0b7e
Mandeep Singh <74090377+mandy-1@users.noreply.github.com>
2022-08-26 19:59:59 +0530

    Update app.js
    added port for heroku deployment

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-//jshint esversion:6
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -75,6 +73,10 @@ app.get("/contact", function(req, res){
 });
 
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+let port = process.env.PORT;
+if(port == null || port == ""){
+  port = 3000;
+}
+app.listen(port, function () {
+  console.log("Server started ");
 });
